76c56d9efff25600943e8a1527dd11576a865898
cpasyanos <pasyanos.c@husky.neu.edu>
2018-04-14 14:27:18 -0400

    IT RENDERS A STATIC VERSION OF THE GAME1git add .git add .git add .

--- TinyEngine/spaceInvaders.py
+++ TinyEngine/spaceInvaders.py
@@ -12,7 +12,8 @@
 # (In fact, run `python3.5-config --includes` in the terminal to see what it does!)
 import mygameengine
 
-MAX_SIZE = 800
+w, h = 11, 5;
+MAX_SIZE = 32 * (w + 2)
 engine = mygameengine.SDLGraphicsProgram(MAX_SIZE,MAX_SIZE)
 
 class Player:
@@ -23,29 +24,49 @@ class Player:
 
     def __init__(self):
         self.x =  MAX_SIZE // 2
-        self.y = 32;
+        self.y = MAX_SIZE - 32;
 
     def draw(self):
-        # engine.SetColor(255, 255, 255, 255);
-        # engine.DrawRectangle(self.x,self.y,self.w,self.h,True);
         engine.DrawImage("sprites/spaceInvader_player.png", self.x, self.y, self.w, self.h);
+    
+    # def move(self, 
 
 
 class Enemy:
+    w = 16
+    h = 16
+    
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        
+    def draw(self):
+        engine.DrawImage("sprites/spaceInvader.png", self.x, self.y, self.w, self.h);
 
 #class Projectile:
 #    def __init__(self):
+
+
+enemyMatrix = [[Enemy(0,0) for x in range(w)] for y in range(h)]
+
+for x in range(0, w):
+    for y in range(0, h):
+        enemyMatrix[y][x] = Enemy(16 + x*32, 64 + 32*y)
 
 player = Player()
 
 print("Setting up game loop")
 engine.SetFramerate(90);
 while not engine.pressed("q") :
+
+    engine.SetBackgroundColor(0, 0, 0, 255)
+	
     # Clear the screen
     engine.clear();
+    
+    for x in range(0, w):
+        for y in range(0, h):
+            enemyMatrix[y][x].draw();
 
     player.draw();
 
