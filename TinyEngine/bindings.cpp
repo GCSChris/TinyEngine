7f3a1ad858841809511bb52ae012aeaf420a3dbc
GCSChris <chrisd1997@gmail.com>
2018-04-16 15:18:18 -0400

    fixed memory leak in resource manager getIMGDimensions

--- TinyEngine/bindings.cpp
+++ TinyEngine/bindings.cpp
@@ -263,9 +263,7 @@ void SDLGraphicsProgram::SetColor(int r, int g, int b, int a) {
 
 void SDLGraphicsProgram::DrawImage(std::string imgPath, int x, int y, int w, int h) {
   SDL_Texture* texture = ResourceManager::instance().getTexture(imgPath, gRenderer);
-
 	SDL_Rect dest = { x, y, w, h };
-
   SDL_RenderCopy(gRenderer, texture, NULL, &dest);
 }
 
@@ -283,10 +281,12 @@ void SDLGraphicsProgram::DrawFrame(std::string imgPath, int frameTick, int sprit
   int currentFrame = frameTick * spriteNumFrames / framerate;
 
   int numColumns = getNumColumns(imgPath, frameWidth);
+
   int frameRectX = (currentFrame % numColumns) * frameWidth;
   int frameRectY = (currentFrame / numColumns) * frameHeight;
   SDL_Rect src = { frameRectX, frameRectY, frameWidth, frameHeight };
   SDL_Rect dest = { x, y, frameWidth, frameHeight };
+
   SDL_RenderCopy(gRenderer, texture, &src, &dest);
 }
 
