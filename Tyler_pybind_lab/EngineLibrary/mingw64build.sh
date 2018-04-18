1d02fac86a0ca89d9e2254782f3bb8f23deb30fc
GCSChris <chrisd1997@gmail.com>
2018-04-18 12:34:07 -0400

    delete chris and tyler's lab

--- Tyler_pybind_lab/EngineLibrary/mingw64build.sh
+++
@@ -1,9 +0,0 @@
-# Run with sh mingw64build.sh
-
-
-COMPILE="g++ -D MINGW -std=c++14 -shared -fPIC -static-libgcc -static-libstdc++ -I./include/ -I./pybind11/include/ `python3 -m pybind11 --includes` *.cpp -o mygameengine.pyd `python3-config --ldflags` -lmingw32 -lSDL2main -lSDL2 -mwindows -L libwinpthread-1.dll"
-
-
-echo $COMPILE
-echo "-------------------------------------------"
-eval $COMPILE
